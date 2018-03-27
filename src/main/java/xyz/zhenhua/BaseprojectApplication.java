ede89356803ee030c2954b5159482ace40e01203
Zachary <wang@zhenhua.xyz>
2018-03-27 22:26:05 +0800

    Initial commit

---
+++ src/main/java/xyz/zhenhua/BaseprojectApplication.java
@@ -0,0 +1,12 @@
+package xyz.zhenhua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseprojectApplication.class, args);
+	}
+}
